4398fb994147b38ea41173606d6009c0c92cd2c9
Yuvraj Choudhary <146911004+Kharbas01@users.noreply.github.com>
2025-09-05 13:53:46 +0530

    Update Car Racing Game.cpp

--- Car Racing Game.cpp
+++ Car Racing Game.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-// A helper function to draw the car on its track
+
 void drawCar(char carName, int position) {
     for (int i = 0; i < position; ++i) {
         cout << " ";
@@ -25,31 +25,29 @@ int main() {
     this_thread::sleep_for(chrono::seconds(1));
 
     for (int round = 1; round <= 15; ++round) {
-        // Clear the screen for a better animation effect
-        // (system("cls") for Windows, system("clear") for Linux/Mac)
+       
         system("cls"); 
         
         cout << "Round " << round << endl;
         
-        // Move the cars: -1 (back), 0 (stay), or 1 (forward)
+        
         posA += (rand() % 3) - 1;
         posB += (rand() % 3) - 1;
 
-        // Ensure cars don't go behind the starting line
+       
         if (posA < 0) posA = 0;
         if (posB < 0) posB = 0;
 
-        // Draw the cars
         drawCar('A', posA);
         drawCar('B', posB);
         cout << "------------------------------------------" << endl;
 
 
-        // Pause for a short duration
+       
         this_thread::sleep_for(chrono::milliseconds(300)); // Corrected syntax
     }
 
-    // Announce the results
+   
     cout << "\n--- RACE OVER ---\n";
     cout << "Car A finished at position: " << posA << endl;
     cout << "Car B finished at position: " << posB << endl;
